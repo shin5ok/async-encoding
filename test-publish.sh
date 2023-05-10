d0286eae9b036163c2aa368b7c99efb074c03b97
shin5ok <shin5ok@55mp.com>
2023-05-10 15:22:21 +0900

    take care of multi OS

--- test-publish.sh
+++ test-publish.sh
@@ -1,7 +1,12 @@
 
 while :
 do
-    uuid=`uuidgen`
+    if which uuid
+    then
+        uuid=`uuid`
+    else
+        uuid=`uuidgen`
+    fi
     message="{\"src\":\"test-1.mp4\",\"dst\":\"bar.mp4\",\"user_id\":\"${uuid}\",\"start\":200.0,\"end\":230.0}"
     echo $message
     gcloud pubsub topics publish --message=$message test
